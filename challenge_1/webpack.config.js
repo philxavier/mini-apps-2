0b494d9eef34e131255e719ee777baac9f805bb6
Felipe <philipe.s.xavier@gmail.com>
2019-05-17 09:22:14 -0700

    finished setting up webpack

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './client/app.jsx',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+                presets:['@babel/preset-env',  "@babel/preset-react"]
+            }
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+      path: __dirname + '/public',
+      filename: 'bundle.js'
+    },
+};
