7ede5649a4d551fe50cd850eb2e06447ac6b23b4
Felipe <philipe.s.xavier@gmail.com>
2019-05-21 14:15:55 -0700

    started creating line graph

--- challenge_2/client/components/Test.jsx
+++ challenge_2/client/components/Test.jsx
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Line } from "react-chartjs-2";
 
 export default class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        labels: [
+          "Boston",
+          "worcester",
+          "Springfield",
+          "Lowell",
+          "Cambridge",
+          "New Bedford"
+        ]
+      }
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("/prices")
+      .then(data => {
+        console.log("this is data", data);
+      })
+      .catch(err => {
+        console.log("there was an error", err);
+      });
+  }
+
   render() {
-    return <div>this is a test</div>;
+    return <Line data={this.state.data} options={{ responsive: true }} />;
   }
 }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,25 @@
 let express = require('express');
 let app = express();
-let port = 3002
+let port = 3002;
+let rp = require('request-promise')
+let bodyParser = require('body-parser');
+let morgan = require('morgan')
 
 app.use(express.static('./public'))
+app.use(morgan('dev'))
+app.use(bodyParser.json());
 
 app.listen(port, () =>
     console.log('Example app listening on port 3002!'),
 );
+
+app.get('/prices', (req, res) => {
+    console.log('the request is here')
+    rp.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-04-01&end=2019-04-30')
+        .then((data) => {
+            console.log('this is data', data)
+        })
+        .catch((err) => {
+            console.log('there was an error', err)
+        })
+})
