6487ee76c9fe0e71830282373af8e338ce6ecf9d
Felipe <philipe.s.xavier@gmail.com>
2019-05-21 17:31:12 -0700

    rendering graph on screen with bitcoin prices for the month of aprin in usd

--- challenge_2/client/components/Test.jsx
+++ challenge_2/client/components/Test.jsx
@@ -7,14 +7,8 @@ export default class Test extends Component {
     super(props);
     this.state = {
       data: {
-        labels: [
-          "Boston",
-          "worcester",
-          "Springfield",
-          "Lowell",
-          "Cambridge",
-          "New Bedford"
-        ]
+        labels: null,
+        datasets: null
       }
     };
   }
@@ -23,14 +17,32 @@ export default class Test extends Component {
     axios
       .get("/prices")
       .then(data => {
-        console.log("this is data", data);
+        // console.log("this is data", data);
+        let targetProperty = data.data.bpi;
+        let labels = Object.keys(targetProperty);
+        let prices = Object.values(targetProperty);
+        this.setState({
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                label: "Price of Bitcoin in USD",
+                data: prices
+              }
+            ]
+          }
+        });
       })
       .catch(err => {
-        console.log("there was an error", err);
+        console.log("there was an error retrieving info from server", err);
       });
   }
 
   render() {
-    return <Line data={this.state.data} options={{ responsive: true }} />;
+    return (
+      <div style={{ width: "40%", height: "40%" }}>
+        <Line data={this.state.data} options={{ responsive: true }} />
+      </div>
+    );
   }
 }
