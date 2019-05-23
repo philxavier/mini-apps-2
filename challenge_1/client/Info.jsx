2b51d2325be3334c8a95ac072f6732777e6533e3
Felipe <philipe.s.xavier@gmail.com>
2019-05-23 14:37:24 -0700

    will read the description sent by json server

--- challenge_1/client/Info.jsx
+++ challenge_1/client/Info.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
+import { Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 export default class Info extends Component {
   constructor(props) {
     super(props);
+    this.myRef = React.createRef();
+
+    this.handleEdit = this.handleEdit.bind(this);
   }
+
+  handleEdit(description) {
+    console.log(description);
+  }
+
+  handleSave() {}
   render() {
     let { data } = this.props;
     // console.log("this is props", data);
@@ -16,8 +26,14 @@ export default class Info extends Component {
               <h3>Date</h3>
               <p>{ele.date}</p>
               <h3>Description</h3>
-              <p>{ele.description}</p>
-              <Button id="buttonEdit" color="danger">
+              <p ref={this.myRef}>{ele.description}</p>
+              <Button
+                onClick={() => {
+                  this.handleEdit(ele.description);
+                }}
+                id="buttonEdit"
+                color="danger"
+              >
                 Edit
               </Button>
               <Button id="buttonSave" color="info">
